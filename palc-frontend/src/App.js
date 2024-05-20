9e7796df877f571be3c2fcc83e41644f6bf11daa
Nikita <personalnick@yandex.ru>
2024-05-20 11:32:28 +0300

    Initial commit with Create React App setup

---
+++ palc-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Palc Music Band</h1>
+        <p>Welcome to our official website!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
